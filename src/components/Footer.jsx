7387f2fd9fb0888f89aca56adc9fdfd66fae0927
MateusRamos <mateus.ramos@gsuite.iff.edu.br>
2025-05-06 17:04:03 -0300

    Agendamento de sala

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,20 +7,20 @@ export default function Footer() {
   const isActive = (path) => pathname === path ? "text-gray-500" : "text-gray-400";
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full h-[6vh] bg-white border-t shadow-md flex justify-around items-center z-20">
+    <footer className="fixed bottom-0 left-0 w-full h-12 bg-white border-t shadow-md flex justify-around items-center z-20">
       <Link to="/" className={`flex flex-col items-center ${isActive("/")}`}>
         <Home size={24} />
       </Link>
       <Link to="/search" className={`flex flex-col items-center ${isActive("/search")}`}>
         <Search  size={24} />
       </Link>
-      <Link to="/" className={`flex flex-col items-center ${isActive("/chat")}`}>
+      <Link to="/chat" className={`flex flex-col items-center ${isActive("/chat")}`}>
         <MessageSquareDot  size={24} />
       </Link>
-      <Link to="/search" className={`flex flex-col items-center ${isActive("/agenda")}`}>
+      <Link to="/agenda" className={`flex flex-col items-center ${isActive("/agenda")}`}>
         <CalendarDays  size={24} />
       </Link>
-      <Link to="/" className={`flex flex-col items-center ${isActive("/perfil")}`}>
+      <Link to="/perfil" className={`flex flex-col items-center ${isActive("/perfil")}`}>
         <User   size={24} />
       </Link>
     </footer>
